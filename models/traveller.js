18a8efd48cb1a0429d4073fadeb4f1d780bdb7a0
Ethan Radcliffe <ethanr@mac.com>
2019-02-20 19:43:41 +0000

    week6 day 3 hw

---
+++ models/traveller.js
@@ -0,0 +1,48 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+// };
+
+// Traveller.prototype.getJourneyStartLocations = function() {
+//   const journeys = [];
+//   this.journeys.startLocation.push();
+//
+//   });
+// return journeys;
+// }
+
+Traveller.prototype.getJourneyEndLocations = function () {
+
+// };
+
+// Traveller.prototype.getJourneysByTransport = function (transport) {
+//   const method = this.journeys.transport.filter( function (journey) {
+//     return journey.transport === transport
+//   });
+// return method;
+// }
+
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+// const found = this.journeys.distance.filter(function(journey) {
+// return journey.distance > minDistance
+//   });
+//
+// return found;
+//
+// }
+
+Traveller.prototype.calculateTotalDistanceTravelled = function (distances) {
+
+  // const total = this.journeys.distance.reduce(function(total, distance) {
+  //   return total + distance;
+  // });
+  //
+  // return total;
+  // }
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+// };
+
+
+module.exports = Traveller;
